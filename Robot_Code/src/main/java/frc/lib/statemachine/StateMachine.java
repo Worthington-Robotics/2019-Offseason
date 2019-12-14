869067acff876c436f5161c4f3fd02dc12ae11d5
coalman321 <coalman321@users.noreply.github.com>
2019-12-14 16:56:55 -0500

    more comments

--- Robot_Code/src/main/java/frc/lib/statemachine/StateMachine.java
+++ Robot_Code/src/main/java/frc/lib/statemachine/StateMachine.java
@@ -19,24 +19,33 @@ public class StateMachine {
 
     private static final Runnable Man = () -> {
         try {
+            //run the onStart code from the descriptor
             descriptor.onStart();
+
+            //state goes to 0 to start
             ConcurrentLinkedQueue<ActionGroup> queuedStates = descriptor.getStates();
             state.set(0);
             SmartDashboard.putNumber("StateMachine/state", state.get());
             if (queuedStates == null) {
+                //a null state queue has been passed
+                //need to set conditions and exit major loop
                 state.set(-2);
                 SmartDashboard.putNumber("StateMachine/state", state.get());
             } else {
                 while (!queuedStates.isEmpty() && !wantStop.get()) {
+                    //pull the next element from the queue and run the state
                     SmartDashboard.putNumber("StateMachine/state", state.get());
                     currentState = queuedStates.poll();
                     currentState.onStart();
+
+                    //wait for the state to complete exectuing
                     while (!currentState.isFinished() && !wantStop.get()) {
                         t_start = Timer.getFPGATimestamp();
-                        System.out.println(t_start);
                         currentState.onLoop();
                         Timer.delay(delay - (Timer.getFPGATimestamp() - t_start));
                     }
+
+                    //when complete stop the state and increment the state counter
                     currentState.onStop();
                     state.getAndAdd(1);
                 }
@@ -46,9 +55,12 @@ public class StateMachine {
             System.out.println(e.getMessage());
             state.set(-3);
         } finally{
+            //run all cleanup procedures
             SmartDashboard.putNumber("StateMachine/state", state.get());
             stateLock.set(false);
             wantStop.set(true);
+
+            //run the onStop code from the descriptor
             descriptor.onStop();
         }
     };
@@ -59,12 +71,21 @@ public class StateMachine {
      * @return true if the machine was started successfully
      */
     public static boolean runMachine(StateMachineDescriptor descrip) {
+        //if the machine is currenly running it must be shut down independently in order to start a new descriptor
         if(stateLock.get()) return false;
+
+        //entered a resettable state
+        //reset the state booleans
         stateLock.set(true);
         wantStop.set(false);
+
+        //move the new state machine in
         descriptor = descrip;
+        
+        //start the new state machine thread
         Thread thread = new Thread(Man);
         thread.start();
+
         return true;
     }
 
@@ -84,7 +105,6 @@ public class StateMachine {
             wantStop.set(true);
             System.out.println("State Machine Halting");
         }
-        
     }
 
 
